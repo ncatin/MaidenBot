1c65762ecba35fade278662060bc8378d93aad6a
Nicholas Catin <the.one146@gmail.com>
2020-08-07 23:23:48 -0400

    stuff

---
+++ MaidenBot/MaidenBot.py
@@ -0,0 +1,26 @@
+import praw
+import pdb
+import re
+import os
+
+reddit = praw.Reddit('bot1')
+subreddit = reddit.subreddit("pythonforengineers")
+
+if not os.path.isfile("posts_replied_to.txt"):
+	posts_replied_to = []
+else:
+	with open("posts_replied_to.txt", "r") as f:
+		posts_replied_to = f.read()
+		posts_replied_to = posts_replied_to.split("\n")
+		posts_replied_to = list(filter(None, posts_replied_to))
+
+for submission in subreddit.hot(limit=5):
+	if submission.id not in posts_replied_to:
+		if re.search("i love python", submission.title, re.IGNORECASE):
+			submission.reply("MaidenBot says: Me too!!")
+			print("Bot replying to: ", submission.title)
+			posts_replied_to.append(submission.id)
+
+		with open("posts_replied_to", "w") as f:
+			for post_id in posts_replied_to:
+				f.write(post_id + "\n")
